4368bb7dd81903dcea368e7bb87a690f5fc479c2
Rahul Rawat <rahulsinghrawat760@gmail.com>
2021-09-14 13:20:00 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var inputText = document.querySelector("#translate-input");
+var outputText = document.querySelector("#translate-output");
+var btnTranslate = document.querySelector("#translate-btn");
+
+var serverUrl = "https://api.funtranslations.com/translate/navi.json";
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function getServerUrl(input) {
+	return serverUrl + "?" + "text=" + input;
+}
+
+function clickHandler() {
+	var textInput = inputText.value;
+
+	fetch(getServerUrl(textInput))
+		.then((response) => response.json())
+		.then((json) => {
+			var translatedText = json.contents.translated;
+			outputText.innerText = translatedText;
+		});
+}
